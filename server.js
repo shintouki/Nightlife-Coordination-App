ac34df38ec33d59955a011a6f43e86ee6277e2d9
Corazon <tdyshin@gmail.com>
2017-01-19 00:21:40 -0500

    Update server.js with express static for jquery, boostrap.js and bootstrap.css

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ var session = require('express-session');
 var bodyParser = require('body-parser');
 
 var app = express();
+
 require('dotenv').load();
 require('./app/config/passport')(passport);
 
@@ -20,6 +21,9 @@ app.set('view engine', 'pug')
 app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 app.use('/common', express.static(process.cwd() + '/app/common'));
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/jquery', express.static(process.cwd() + '/node_modules/jquery/dist'));
+app.use('/bootstrap/js', express.static(process.cwd() + '/node_modules/bootstrap/dist/js'));
+app.use('/bootstrap/css', express.static(process.cwd() + '/node_modules/bootstrap/dist/css'));
 
 app.use(session({
   secret: process.env.SESSION_SECRET,
