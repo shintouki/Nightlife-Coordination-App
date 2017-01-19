bc721cc284760a4c894d11533e4e4061828e7cf3
Corazon <tdyshin@gmail.com>
2017-01-19 16:52:53 -0500

    Set up barHandler

---
+++ app/controllers/barHandler.server.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var Bars = require('../models/bars.js');
+var Users = require('../models/users.js');
+var appUrl = process.env.APP_URL
+
+function PollHandler() {
+
+  
+
+}
+
+module.exports = PollHandler;
--- app/controllers/clickController.client.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-(function () {
-  // var apiUrl = appUrl + ;
-
-
-
-})();
