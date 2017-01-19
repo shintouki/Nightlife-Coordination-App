27719f9e655d3e441cbd1dc3eedf57f6bbe4489f
Corazon <tdyshin@gmail.com>
2017-01-18 23:48:01 -0500

    Update and set up index.js routes properly

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,34 +1,74 @@
 'use strict';
 
 var path = process.cwd();
-
-var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
+var BarHandler = require(path + '/app/controllers/barHandler.server.js');
 
 module.exports = function(app, passport) {
 
   function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
       return next();
-    } else {
-      res.redirect('/login');
+    }
+    else {
+      res.redirect('/search');
     }
   }
 
-  var clickHandler = new ClickHandler();
+  var barHandler = new BarHandler();
 
-  app.route('/')
-  .get(isLoggedIn, function(req, res) {
-    res.sendFile(path + '/public/index.html');
+  app.route('*')
+    .get(function(req, res, next) {
+    // Place user into res.locals for easy access from templates
+    res.locals.user = req.user || null;
+    next();
   });
 
-  app.route('/login')
-  .get(function (req, res) {
-    res.sendFile(path + '/public/login.html');
-  });
+  app.route('/search')
+    .get(function (req, res) {
+      res.render('search');
+    });
 
   app.route('/logout')
-  .get(function (req, res) {
-    req.logout();
-    res.redirect('/login');
-  });
+    .get(function (req, res) {
+      req.logout();
+      res.redirect('/search');
+    });
+
+  app.route('/mybars')
+    .get(isLoggedIn, function (req, res) {
+      res.render('my-bars');
+    });
+
+  app.route('/auth/twitter')
+    .get(passport.authenticate('twitter'));
+
+  app.route('/auth/twitter/callback')
+    .get(passport.authenticate('twitter', {
+      successRedirect: '/after-twitter-auth',
+      failureRedirect: '/search'
+    }));
+
+  app.route('/after-twitter-auth')
+    .get(isLoggedIn, function (req, res) {
+      res.render('after-twitter-auth');
+    });
+
+  app.route('/api/allbars')
+    .get(barHandler.getAllBars);
+
+  app.route('/api/:id')
+    .get(isLoggedIn, function (req, res) {
+      res.json(req.user.twitter);
+    });
+
+  app.route('/api/:id/bars')
+    .get(isLoggedIn, barHandler.getUserBars)
+    .post(isLoggedIn, barHandler.addUserBar)
+    .delete(isLoggedIn, barHandler.deleteUserBar);
+
+  app.route('*')
+    .get(function (req, res) {
+      res.redirect('/search');
+    });
+
 };
