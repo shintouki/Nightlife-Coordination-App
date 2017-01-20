60137dbc2e6394acaef8f0dc806e8daa8a7d0730
Corazon <tdyshin@gmail.com>
2017-01-19 22:44:55 -0500

    Make usercontrolelr that shows twitter username when logged in

---
+++ app/controllers/userController.client.js
@@ -0,0 +1,18 @@
+'use strict';
+
+(function () {
+   var displayName = document.querySelector('#display-name');
+   var apiUrl = appUrl + '/api/:id';
+
+   function updateHtmlElement (data, element, userProperty) {
+      element.innerHTML = data[userProperty];
+   }
+
+   ajaxFunctions.ready(ajaxFunctions.ajaxRequest('GET', apiUrl, function (data) {
+      var userObject = JSON.parse(data);
+      if (displayName !== null) {
+         updateHtmlElement(userObject, displayName, 'displayName');   
+      }
+
+   }));
+})();
