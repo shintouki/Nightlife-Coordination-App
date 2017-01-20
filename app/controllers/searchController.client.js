ae5c796f8cf043ad093570fc7de9006f7a25f116
Corazon <tdyshin@gmail.com>
2017-01-20 15:41:33 -0500

    Add search images to page

--- app/controllers/searchController.client.js
+++ app/controllers/searchController.client.js
@@ -17,7 +17,23 @@
     var searchLocation = searchInput.val();
     $.get('/yelp-search', { searchLocation: searchLocation }, function(data) {
       // console.log(data);
-      
+      for (var i = 0; i < data.length; i++) {
+        var rating = data[i].rating;
+        var name = data[i].name;
+        var url = data[i].url;
+        var snippet_text = data[i].snippet_text;
+        var image_url = data[i].image_url;
+        var id = data[i].id;
+
+        var $div = $("<div>", { class: "list-group-item" });
+        var $img = $("<img />", {
+          src: image_url,
+          alt: 'Image of ' + name
+        });
+
+        $div.append($img);
+        searchResult.append($div);
+      }
     }, 'json');
   });
    
